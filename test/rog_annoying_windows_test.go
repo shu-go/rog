57a4c26923aad255b2cfa9a8e2767644925fbe91
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2017-11-23 09:52:21 +0900

    add annoying hook for Windows (msg command)

---
+++ test/rog_annoying_windows_test.go
@@ -0,0 +1,18 @@
+package test
+
+import (
+	"testing"
+
+	"bitbucket.org/shu/rog"
+)
+
+func DoNotTestAnnoyingHook(t *testing.T) {
+	l := rog.Discard.Bind()
+	l.Hook(rog.AnnoyingHook())
+
+	l.Print("Hello?")
+	l.Print("Are you there?")
+
+	// not hooked
+	rog.Discard.Print("I'm not here.")
+}
